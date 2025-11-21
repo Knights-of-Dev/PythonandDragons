121caa0d5f2f604d1bd30d988cf8b30b565d310d
Pitchfork :3 <kanonenwagen7@gmail.com>
2025-11-21 10:18:45 -0500

    Update PythonandDragons.py

--- PythonandDragons/PythonandDragons.py
+++ PythonandDragons/PythonandDragons.py
@@ -5,5 +5,16 @@ This is where all the functions, classes, variables, etc lie in your custom modu
 """
 import random
 
+class player(name, clas, level, race, background, str, dex, con, int, wis, cha, inventorylist):
+  pass
+
+
+
+
 def roll(number):
   return random.randint(1, number)
+
+
+def makechar(name, clas, level, race, background, str, dex, con, int, wis, cha, inventorylist, classlist, racelist):
+  pass
+    
