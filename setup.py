4eef2cfeacea1f7664a76a2c99eb7ce9813e7b4c
Pitchfork :3 <kanonenwagen7@gmail.com>
2025-11-21 13:22:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PythonandDragons',
-    version='0.0.1',    
+    version='0.0.2',    
     description='DND functions in python!',
     url='https://github.com/Knights-of-Dev/PythonandDragons',
     author='Pitchfork7',
