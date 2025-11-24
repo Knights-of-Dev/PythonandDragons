24118c195e35aa19d102fed305e832ab54369b09
Pitchfork :3 <kanonenwagen7@gmail.com>
2025-11-24 08:48:27 -0500

    Update PythonandDragons.py

--- PythonandDragons/PythonandDragons.py
+++ PythonandDragons/PythonandDragons.py
@@ -6,7 +6,7 @@ This is where all the functions, classes, variables, etc lie in your custom modu
 import random
 
 class player():
-  def __init__(self, name, clas, level, race, str, dex, con, int, wis, cha, inventorylist, health, hitdie, xp, proficiencylist, gold, proficiencybonus, size, ac, speed, skills, magic, pasper):
+  def __init__(self, name, clas, level, race, str, dex, con, int, wis, cha, inventorylist, health, hitdie, xp, proficiencylist, gold, proficiencybonus, size, ac, speed, skills, pasper):
     self.name = name
     self.level = level
     self.clas = clas
@@ -36,12 +36,56 @@ class player():
   def charname(self):
     return self.name
 
+  def charclas(self):
+    return self.clas
 
   def charlevel(self):
     return self.level
 
-  def charclas(self):
-    return self.clas
+  def charrace(self):
+    return self.race
+
+  def charstr(self):
+    return self.str
+
+  def chardex(self):
+    return self.dex
+
+  def charcon(self):
+    return self.con
+
+  def charint(self):
+    return self.int
+
+  def charwis(self):
+    return self.wis
+
+  def charcha(self):
+    return self.cha
+    
+  def charinventorylist(self):
+    return self.inventorylist
+
+  def chargold(self):
+    return self.gold
+
+  def charproficiencybonus(self):
+    return self.proficiencybonus
+
+  def charsize(self):
+    return self.size
+
+  def charac(self):
+    return self.ac
+
+  def charspeed(self):
+    return self.speed
+
+  def charskills(self):
+    return self.skills
+
+  def charpasper(self):
+    return self.pasper
 
 
 def roll(number, modifier):
